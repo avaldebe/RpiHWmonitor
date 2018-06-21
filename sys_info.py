204ffe0a3dec369ff1d76f25812c31f64dbc754e
Alvaro Valdebenito <avaldebe@gmail.com>
2018-06-21 20:32:00 +0200

    new layout

--- sys_info.py
+++ sys_info.py
@@ -24,6 +24,8 @@ def stats(device, info, font):
     draw info to canvas
     """
     sinfo = dict(
+        host='{0[1]} {0[2]}'.format(os.uname()),
+#       boot = datetime.fromtimestamp(psutil.boot_time()).strftime("%F %R"),
         ip=socket.gethostbyname('%s.local'%socket.gethostname()),
         cpu='%.0f%%'%(os.getloadavg()[0]*100),
         mem='MEM: %.0f%%'%psutil.virtual_memory().percent,
@@ -35,8 +37,8 @@ def stats(device, info, font):
     )
     with canvas(device) as draw:
         for text, (xy, icon, dxy, size) in info.items():
-            xy0=xy
-            xy1=(xy[0]+dxy[0],xy[1]+dxy[1])
+            xy0 = xy
+            xy1 = (xy[0]+dxy[0],xy[1]+dxy[1])
             draw.text(xy0, icon, font=font['icon_%s'%size], fill="white")
             draw.text(xy1, sinfo[text], font=font['text_%s'%size], fill="white")
 
@@ -54,16 +56,17 @@ def main():
         text_small = ImageFont.truetype(font_path%'Montserrat-Light.ttf', 12),
         text_large = ImageFont.truetype(font_path%'Montserrat-Medium.ttf', 19),
         icon_small = ImageFont.truetype(font_path%'fontawesome-webfont.ttf', 14),
-        icon_large = ImageFont.truetype(font_path%'fontawesome-webfont.ttf', 20),
+        icon_large = ImageFont.truetype(font_path%'fontawesome-webfont.ttf', 19),
     )
 
     # layout
     info = dict(#((x, y), icon, (dx, dy), size)
-        ip   = (( 0,  0), '\uf1eb', (18,  0), 'small'),
-        cpu  = (( 0, 15), '\uf2db', (22, -3), 'large'),
-        mem  = (( 0, 36), ''      , ( 0,  0), 'small'),
-        disk = ((76, 36), '\uf1c0', (13,  0), 'small'),
-        temp = ((78, 18), '\uf2c8', (10,  0), 'small'),
+        host = (( 0,  0), ''      , ( 0,  0), 'small'),
+        ip   = (( 0, 15), '\uf1eb', (18,  0), 'small'),
+        cpu  = (( 0, 29), '\uf2db', (22, -2), 'large'),
+        mem  = (( 0, 50), ''      , ( 0,  0), 'small'),
+        disk = ((76, 50), '\uf1c0', (13,  0), 'small'),
+        temp = ((78, 33), '\uf2c8', (10,  0), 'small'),
     )
 
     while True:
