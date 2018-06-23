255c0a91cf2fc1cb3af29650e3f3ca2a610070a2
Alvaro Valdebenito <avaldebe@gmail.com>
2018-06-23 16:51:10 +0200

    sinfo as a function dispatcher

--- sys_info.py
+++ sys_info.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Display basic system information.
+- host: nodename kernel-release (same as `uname -nr`)
+- ip: IP address
+- wifi: signal strength (RSSI) [dBm]
+- cpu: mean CPU load [%], calulcated as 1 min mean load / number of cores * 100
+- mem: memory usage [%], calculated as (total - available) / total * 100
+- disk: disk usage [%]
+- temp: CPU temperature [°C]
 """
 
 import os
@@ -18,42 +25,48 @@ except ImportError:
 finally:
     from luma.core.render import canvas
 
+def sinfo(key):
+    """
+    Basic system information
+    """
+
+    host = lambda : '{0[1]} {0[2]}'.format(os.uname())
+
+    ip = lambda : socket.gethostbyname('%s.local'%socket.gethostname())
+
+    wifi = lambda : subprocess.check_output(
+        "iwconfig wlan0 2>/dev/null| grep Signal | sed 's:.*=::'",
+        shell=True,
+    ).decode('UTF-8').replace('dBm', '').strip() or 'N/A'
+
+    cpu = lambda : '%.0f%%'%(os.getloadavg()[0]/os.cpu_count()*100)
+
+    mem = lambda : 'MEM: %.0f%%'%psutil.virtual_memory().percent
+
+    disk = lambda : '%.0f%%'%psutil.disk_usage("/").percent
+
+    temp = lambda : subprocess.check_output(
+        """awk '{printf "%.0f°C", $0/1000}' < /sys/class/thermal/thermal_zone0/temp""",
+        shell=True,
+    ).decode('UTF-8')
+
+    try:
+        return dict(host=host, ip=ip, wifi=wifi, cpu=cpu, mem=mem, disk=disk, temp=temp)[key]
+    except KeyError:
+        return None
+
 
 def stats(device, info, font):
     """
     draw system info to canvas
-    - host: nodename kernel-release (same as `uname -nr`)
-    - ip: IP address
-    - wifi: signal strength (RSSI) [dBm]
-    - cpu: mean CPU load [%], calulcated as 1 min mean load / number of cores * 100
-    - mem: memory usage [%], calculated as (total - available) / total * 100
-    - disk: disk usage [%]
-    - temp: CPU temperature [°C]
     """
-    sinfo = dict(
-        host='{0[1]} {0[2]}'.format(os.uname()),
-#       boot = datetime.fromtimestamp(psutil.boot_time()).strftime("%F %R"),
-        ip=socket.gethostbyname('%s.local'%socket.gethostname()),
-        wifi=subprocess.check_output(
-            "iwconfig wlan0 2>/dev/null| grep Signal | sed 's:.*=::'",
-            shell=True,
-        ).decode('UTF-8').replace('dBm', '').strip() or 'N/A',
-        cpu='%.0f%%'%(os.getloadavg()[0]/os.cpu_count()*100),
-        mem='MEM: %.0f%%'%psutil.virtual_memory().percent,
-        disk='%.0f%%'%psutil.disk_usage("/").percent,
-        temp=subprocess.check_output(
-            """awk '{printf "%.0f°C", $0/1000}' < /sys/class/thermal/thermal_zone0/temp""",
-            shell=True,
-        ).decode('UTF-8'),
-    )
     with canvas(device) as draw:
-        for key, (xy, icon, dxy, size) in info.items():
-            xy0 = xy
-            xy1 = (xy[0]+dxy[0],xy[1]+dxy[1])
+        for (x, y, icon, dx, dy, size, fn) in info:
             if icon:
-                draw.text(xy0, icon, font=font['icon_%s'%size], fill="white")
-            if sinfo[key]:
-                draw.text(xy1, sinfo[key], font=font['text_%s'%size], fill="white")
+                draw.text((x, y), icon, font=font['icon_%s'%size], fill="white")
+            if fn:
+                x += dx; y += dy
+                draw.text((x, y), fn(), font=font['text_%s'%size], fill="white")
 
 
 def main():
@@ -72,15 +85,15 @@ def main():
         icon_large = ImageFont.truetype(font_path%'fontawesome-webfont.ttf', 19),
     )
 
-    # layout
-    info = dict(#((x, y), icon, (dx, dy), size)
-        host = (( 1,  0), None    , ( 0,  0), 'small'),
-        ip   = (( 1, 15), None    , ( 0,  0), 'small'),
-        cpu  = (( 0, 29), '\uf2db', (22, -2), 'large'),
-        mem  = (( 0, 50), None    , ( 0,  0), 'small'),
-        wifi = ((74, 15), '\uf1eb', (18,  0), 'small'),
-        disk = ((76, 50), '\uf1c0', (13,  0), 'small'),
-        temp = ((78, 33), '\uf2c8', (10,  0), 'small'),
+    # layout: (x, y, icon, dx, dy, size, function)
+    info = (
+        ( 1,  0, None    ,  0,  0, 'small', sinfo('host')),
+        ( 1, 15, None    ,  0,  0, 'small', sinfo('ip')),
+        ( 0, 29, '\uf2db', 22, -2, 'large', sinfo('cpu')),
+        ( 0, 50, None    ,  0,  0, 'small', sinfo('mem')),
+        (74, 15, '\uf1eb', 18,  0, 'small', sinfo('wifi')),
+        (76, 50, '\uf1c0', 13,  0, 'small', sinfo('disk')),
+        (78, 33, '\uf2c8', 10,  0, 'small', sinfo('temp')),
     )
 
     while True:
